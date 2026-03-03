3f7e77dc3153a7d210d38fa9c802f8d124e70425
AnatoliiBuhrov <abu@covernet.de>
2026-03-03 02:31:22 +0200

    add new translations and commentaries, add new vocabulary and cross references

--- composeApp/src/commonMain/kotlin/com/abuhrov/openword/data/config/TranslationConfig.kt
+++ composeApp/src/commonMain/kotlin/com/abuhrov/openword/data/config/TranslationConfig.kt
@@ -5,22 +5,34 @@ import com.abuhrov.openword.model.Translation
 
 val availableTranslations = listOf(
     Translation(
-        id = "CUV",
+        id = "GRM",
+        displayName = "Біблія: Вічне Євангеліє",
+        fileName = "translations/GRM.SQLite3",
+        commentarySource = CommentarySource("Біблія: Вічне Євангеліє", "commentaries/GRM.commentaries.SQLite3")
+    ),
+    Translation(
+        id = "CUV'23",
         displayName = "Сучасний переклад УБТ",
         fileName = "translations/CUV.SQLite3",
         commentarySource = CommentarySource("Сучасний переклад", "commentaries/CUV.commentaries.SQLite3")
     ),
-    Translation("GYZ", "Олександр Гижа", "translations/GYZ.SQLite3"),
     Translation("HOM", "Іван Хоменко", "translations/HOM.SQLite3"),
+    Translation("UKRK", "Куліша, Пулюя та Нечуя-Левицького", "translations/UKRK.SQLite3"),
     Translation("KJV", "King James", "translations/KJV.SQLite3"),
-    Translation("МСЦ", "МСЦ ЄХБ", "translations/MSC.SQLite3"),
+    Translation("МСЦ'22", "МСЦ ЄХБ", "translations/MSC.SQLite3"),
     Translation(
         id = "UBIO",
         displayName = "Іван Огієнко",
         fileName = "translations/UBIO.SQLite3",
         commentarySource = CommentarySource("Іван Огієнко", "commentaries/UBIO.commentaries.SQLite3")
     ),
-    Translation("NUP", "Юрій Попченко", "translations/NUP.SQLite3"),
+    Translation("НУП", "Юрій Попченко", "translations/NUP.SQLite3"),
+    Translation(
+        id = "НПУ'22",
+        displayName = "Новий переклад українською",
+        fileName = "translations/NPU.SQLite3",
+        commentarySource = CommentarySource("Новий переклад українською", "commentaries/NPU.commentaries.SQLite3")
+    ),
     Translation(
         id = "UMT",
         displayName = "Свята Біблія: Сучасною мовою",
